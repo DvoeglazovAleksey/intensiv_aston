f52caa0140c67910568ccfe64e2969009c9f5360
Aleksey_Dvoeglazov Java_Developer <alekdvoeglazov@yandex.ru>
2024-02-02 14:12:02 +0300

    Refactor: spring in spring boot.
    Refactor: spring in spring boot.

--- src/test/java/org/example/user/UserServiceImplTest.java
+++ src/test/java/org/example/user/UserServiceImplTest.java
@@ -8,13 +8,16 @@ import org.example.repository.UserRepository;
 import org.example.service.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class UserServiceImplTest {
     private final UserRepository userRepository;
     private final UserServiceImpl userService;
@@ -33,15 +36,15 @@ public class UserServiceImplTest {
         expectedUser = new User(1L, "user", "firstName", "lastName", "password");
     }
 
+
     @Test// Добавляем юзера = проверяем сохранился ли.
     public void addNewUser_valid() {
         when(userRepository.findByLogin(createUserDto.getLogin())).thenReturn(null);
-        User user = UserMapper.createUserDtoToUser(createUserDto);
-        when(userRepository.addAndUpdate(user)).thenReturn(expectedUser);
+        when(userRepository.save(UserMapper.createUserDtoToUser(createUserDto))).thenReturn(expectedUser);
 
         ResultUserDto actualUserDto = userService.create(createUserDto);
 
-        verify(userRepository).addAndUpdate(expectedUser);
+        verify(userRepository).save(expectedUser);
         assertEquals(actualUserDto.getLogin(), expectedUser.getLogin());
     }
 
